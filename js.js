1243a844a18b3bdc28b3cd63358340bc51e325a1
rohit <rohtashsinghyadav2002@gmail.com>
2023-11-11 08:17:41 +0530

    this is the todo list first commit

---
+++ js.js
@@ -0,0 +1,4 @@
+const feedback=document.querySelector('.feedback')
+const btn=document.querySelector(".btn")
+const input=document.querySelector("#input-box")
+const list=document.querySelector("#list");
